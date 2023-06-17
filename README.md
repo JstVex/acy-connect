21171209c1db78e70b5b4d822b4ae01d69d8794a
JstVex <heinhtetlulu14@gmail.com>
2023-06-18 00:52:00 +0630

    Initialize the app

---
+++ README.md
@@ -0,0 +1,1 @@
+## Welcome all acy members
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='text-blue-400'>
+      ACY
+    </div>
+  )
+}
+
+export default App
