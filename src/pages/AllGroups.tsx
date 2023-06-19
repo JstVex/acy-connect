f3bf6f3e188b1b7c0cf556cc27ae32cedc5eec51
JstVex <heinhtetlulu14@gmail.com>
2023-06-19 23:45:36 +0630

    Add seacrhbar, new group creation button and display all groups

--- src/pages/AllGroups.tsx
+++ src/pages/AllGroups.tsx
@@ -1,11 +1,41 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
+import SearchBar from '../components/all-groups/SearchBar'
+import CreateNewGroup from '../components/all-groups/CreateNewGroup'
+import Group from '../components/Group'
 
 interface AllGroupsProps {
-    props: string
+    props?: string
 }
 
 const AllGroups: FC<AllGroupsProps> = ({ props }) => {
-    return <div>AllGroups</div>
+    const [groups, setGroups] = useState([]);
+
+    useEffect(() => {
+        const fetchGroups = async () => {
+            const response = await fetch('http://localhost:4080/groups')
+            const data = await response.json();
+
+            if (response.ok) {
+                setGroups(data)
+            }
+        }
+
+        fetchGroups()
+    }, [groups])
+
+    return (
+        <div className='w-full p-3'>
+            <div className='flex gap-x-4'>
+                <SearchBar />
+                <CreateNewGroup />
+            </div>
+            <ul>
+                {groups.map((group) => {
+                    return <Group group={group} />
+                })}
+            </ul>
+        </div>
+    )
 }
 
 export default AllGroups
