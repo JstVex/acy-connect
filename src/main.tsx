356126f929523bc05f22714af4eb7fa00928ff56
JstVex <heinhtetlulu14@gmail.com>
2023-06-19 23:41:19 +0630

    Wrap root with auth context provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 // import App from './App.tsx'
 import './index.css'
 import AppRouter from './router/AppRouter.tsx'
+import { AuthContextProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AppRouter />
+    <AuthContextProvider>
+      <AppRouter />
+    </AuthContextProvider>
   </React.StrictMode>,
 )
