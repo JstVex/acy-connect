22371eded4573b700280d5b357c644539d164035
JstVex <heinhtetlulu14@gmail.com>
2023-06-21 23:01:10 +0630

    Protect all routes except signin

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,20 +1,43 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import ProtectedRoute from './ProtectRoutes';
 import AllGroups from '../pages/AllGroups';
 import Sidebar from '../components/Sidebar';
 import JoinedGroups from '../pages/JoinedGroups';
 import Connections from '../pages/Connections';
 import Events from '../pages/Events';
+import Signin from '../pages/Signin';
 
 function AppRouter() {
+    const { user } = useContext(AuthContext);
+
     return (
         <Router>
             <div className='flex'>
-                <Sidebar />
+                {user && <Sidebar />}
                 <Routes>
-                    <Route path="/all-groups" element={<AllGroups props={'hi'} />} />
-                    <Route path="/joined-groups" element={<JoinedGroups />} />
-                    <Route path="connections" element={<Connections />} />
-                    <Route path="events" element={<Events />} />
+                    <Route path="/all-groups" element={
+                        <ProtectedRoute>
+                            <AllGroups />
+                        </ProtectedRoute>}
+                    />
+                    <Route path="/joined-groups" element={
+                        <ProtectedRoute>
+                            <JoinedGroups />
+                        </ProtectedRoute>}
+                    />
+                    <Route path="/connections" element={
+                        <ProtectedRoute>
+                            <Connections />
+                        </ProtectedRoute>}
+                    />
+                    <Route path="/events" element={
+                        <ProtectedRoute>
+                            <Events />
+                        </ProtectedRoute>}
+                    />
+                    <Route path="/signin" element={<Signin />} />
                 </Routes>
             </div>
         </Router>
