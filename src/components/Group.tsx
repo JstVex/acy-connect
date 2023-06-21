dfb576b3417f081fc3e95997949ef5be5960de61
JstVex <heinhtetlulu14@gmail.com>
2023-06-21 23:04:32 +0630

    Find the matching user to find the group's owner

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,11 +1,23 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import Event from './Event'
+import { UserModel } from '../types/models'
+import { GroupModel } from '../types/models'
 
 interface GroupProps {
-    group: any
+    group: GroupModel,
+    users: UserModel[]
 }
 
-const Group: FC<GroupProps> = ({ group }) => {
+const Group: FC<GroupProps> = ({ group, users }) => {
+    const [ownerName, setOwnerName] = useState('');
+
+    useEffect(() => {
+        const owner = users.find((user) => user._id === group.owner);
+        if (owner) {
+            setOwnerName(owner.name);
+        }
+    }, [group.owner, users]);
+
     return (
         <li className='flex flex-col ring-1 ring-zinc-900 rounded-md shadow-md my-5 p-3'>
             <div className='flex items-center '>
@@ -13,7 +25,7 @@ const Group: FC<GroupProps> = ({ group }) => {
                     {group.title}
                 </h3>
                 <div className='font-light'>
-                    Created by {group.owner}
+                    Created by {ownerName}
                 </div>
             </div>
             <p className='my-1'>
