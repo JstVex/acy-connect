3061a996682bdf88e2f7ebd5d2f9014eded9aae8
JstVex <heinhtetlulu14@gmail.com>
2023-06-21 23:04:04 +0630

    Fetch all users as well to prop it down to Group component

--- src/pages/AllGroups.tsx
+++ src/pages/AllGroups.tsx
@@ -2,13 +2,14 @@ import { FC, useEffect, useState } from 'react'
 import SearchBar from '../components/all-groups/SearchBar'
 import CreateNewGroup from '../components/all-groups/CreateNewGroup'
 import Group from '../components/Group'
+import { GroupModel } from '../types/models'
 
 interface AllGroupsProps {
     props?: string
 }
-
-const AllGroups: FC<AllGroupsProps> = ({ props }) => {
-    const [groups, setGroups] = useState([]);
+const AllGroups: FC<AllGroupsProps> = () => {
+    const [groups, setGroups] = useState<GroupModel[] | null>(null);
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
         const fetchGroups = async () => {
@@ -20,18 +21,28 @@ const AllGroups: FC<AllGroupsProps> = ({ props }) => {
             }
         }
 
-        fetchGroups()
+        const fetchUsers = async () => {
+            const response = await fetch('http://localhost:4080/users')
+            const data = await response.json();
+
+            if (response.ok) {
+                setUsers(data)
+            }
+        }
+
+        fetchGroups();
+        fetchUsers();
     }, [groups])
 
     return (
-        <div className='w-full p-3'>
+        <div className='w-full max-h-screen overflow-y-auto p-3'>
             <div className='flex gap-x-4'>
                 <SearchBar />
                 <CreateNewGroup />
             </div>
             <ul>
-                {groups.map((group) => {
-                    return <Group group={group} />
+                {groups?.map((group) => {
+                    return <Group key={group._id} group={group} users={users} />
                 })}
             </ul>
         </div>
