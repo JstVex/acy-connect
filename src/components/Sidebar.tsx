d0a3d1e7b8d15eef34ac28cb58c58b17d7c4a759
JstVex <heinhtetlulu14@gmail.com>
2023-06-23 23:19:44 +0630

    Fetch the current user to display their image

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,41 @@
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import { UserModel } from '../types/models';
 
 function Sidebar() {
     const { logout } = useContext(AuthContext);
+    const [user, setUser] = useState<UserModel | null>(null)
 
     const handleLogout = () => {
         logout();
     }
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const token = localStorage.getItem('token')
+                const response = await fetch('http://localhost:4080/users/me', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`,
+                    },
+                });
+                const user = await response.json();
+
+                if (response.ok) {
+                    setUser(user);
+                }
+
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        }
+
+        fetchUser();
+    }, [])
+
     return (
         <nav className='w-60 h-screen bg-amber-100 p-3 flex flex-col'>
             <h4 className='mb-5'>
@@ -27,9 +55,16 @@ function Sidebar() {
                     <Link to="/events">Events</Link>
                 </li>
             </ul>
-            <div className='mt-auto' onClick={handleLogout}>
-                Profile
-            </div>
+            <Link className='mt-auto flex items-center gap-x-3' to="/me">
+                {user?.image ? (
+                    <img src={user?.image} alt="pfp" className='w-auto h-auto max-w-[48px] max-h-[48px] aspect-square rounded-full' />
+                ) : (
+                    <img src="/src/assests/placeholder.jpeg" alt="placeholder" className='w-auto h-auto max-w-[48px] max-h-[48px] aspect-square rounded-full' />
+                )}
+                <div className='text-lg'>
+                    {user?.name}
+                </div>
+            </Link>
         </nav>
     );
 }
