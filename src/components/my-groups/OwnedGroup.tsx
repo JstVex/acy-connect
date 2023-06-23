03d3d8050e828bc06a665feae29626c0d311dcc5
JstVex <heinhtetlulu14@gmail.com>
2023-06-23 23:23:59 +0630

    Display joined and owned groups

---
+++ src/components/my-groups/OwnedGroup.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react'
+import { GroupModel } from '../../types/models'
+import { Settings } from 'lucide-react'
+
+interface OwnedGroupProps {
+    group: GroupModel
+}
+
+const OwnedGroup: FC<OwnedGroupProps> = ({ group }) => {
+    return (
+        <li className='flex flex-col ring-1 ring-zinc-900 rounded-md shadow-md my-5 p-3'>
+            <div className='flex items-center '>
+                <h3 className='text-lg flex-grow'>
+                    {group.title}
+                </h3>
+                <Settings />
+            </div>
+            <p className='my-1'>
+                {group.description}
+            </p>
+            <div className='text-sm'>
+                {group.members.length} members including mutual connections
+            </div>
+            <div className='text-sm'>
+                Usually active from {group.time} on {group.date} at {group.place}
+            </div>
+            {/* <div>
+                On going events
+            </div>
+            <div>
+                {group.events.map((event: any) => {
+                    return <Event event={event} />
+                })}
+            </div> */}
+        </li>
+    )
+}
+
+export default OwnedGroup
--- src/pages/MyGroups.tsx
+++ src/pages/MyGroups.tsx
@@ -1,12 +1,14 @@
 import { FC, useEffect, useState } from 'react'
 import SearchBar from '../components/all-groups/SearchBar'
 import { GroupModel } from '../types/models'
+import OwnedGroup from '../components/my-groups/OwnedGroup'
+import JoinedGroup from '../components/my-groups/JoinedGroup'
 
 interface MyGroupsProps {
     props?: string
 }
 
-const MyGroups: FC<MyGroupsProps> = ({ props }) => {
+const MyGroups: FC<MyGroupsProps> = () => {
     const [ownedGroups, setOwnedGroups] = useState<GroupModel[]>([]);
     const [joinedGroups, setJoinedGroups] = useState<GroupModel[]>([]);
 
@@ -56,7 +58,25 @@ const MyGroups: FC<MyGroupsProps> = ({ props }) => {
             <h3 className='text-xl mt-5'>
                 Owned Groups
             </h3>
-
+            {ownedGroups.length === 0 && (
+                <div className='mt-3 my-10 font-light'>You haven't created any group yet.</div>
+            )}
+            <ul>
+                {ownedGroups.map((ownedGroup) => {
+                    return <OwnedGroup key={ownedGroup._id} group={ownedGroup} />
+                })}
+            </ul>
+            <h3 className='text-xl mt-5'>
+                Joined Groups
+            </h3>
+            {joinedGroups.length === 0 && (
+                <div className='mt-3 my-10 font-light'>You haven't joined any group yet.</div>
+            )}
+            <ul>
+                {joinedGroups.map((joinedGroup) => {
+                    return <JoinedGroup key={joinedGroup._id} group={joinedGroup} />
+                })}
+            </ul>
         </div>
     )
 }
