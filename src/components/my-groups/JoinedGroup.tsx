583f08fdf9f2faa4c5abdb9ae61c6abf4b495c79
JstVex <heinhtetlulu14@gmail.com>
2023-06-26 21:55:10 +0630

    Link to group profile page on click

--- src/components/my-groups/JoinedGroup.tsx
+++ src/components/my-groups/JoinedGroup.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import { GroupModel } from '../../types/models'
+import { Link } from 'react-router-dom'
 
 interface JoinedGroupProps {
     group: GroupModel
@@ -7,22 +8,23 @@ interface JoinedGroupProps {
 
 const JoinedGroup: FC<JoinedGroupProps> = ({ group }) => {
     return (
-        <li className='flex flex-col ring-1 ring-zinc-900 rounded-md shadow-md my-5 p-3'>
-            <div className='flex items-center '>
-                <h3 className='text-lg flex-grow'>
-                    {group.title}
-                </h3>
-            </div>
-            <p className='my-1'>
-                {group.description}
-            </p>
-            <div className='text-sm'>
-                {group.members.length} members including mutual connections
-            </div>
-            <div className='text-sm'>
-                Usually active from {group.time} on {group.date} at {group.place}
-            </div>
-            {/* <div>
+        <Link to={`/groups/${group._id}`}>
+            <li className='flex flex-col ring-1 ring-zinc-900 rounded-md shadow-md my-5 p-3'>
+                <div className='flex items-center '>
+                    <h3 className='text-lg flex-grow'>
+                        {group.title}
+                    </h3>
+                </div>
+                <p className='my-1'>
+                    {group.description}
+                </p>
+                <div className='text-sm'>
+                    {group.members.length} members including mutual connections
+                </div>
+                <div className='text-sm'>
+                    Usually active from {group.time} on {group.date} at {group.place}
+                </div>
+                {/* <div>
                 On going events
             </div>
             <div>
@@ -30,7 +32,8 @@ const JoinedGroup: FC<JoinedGroupProps> = ({ group }) => {
                     return <Event event={event} />
                 })}
             </div> */}
-        </li>
+            </li>
+        </Link>
     )
 }
 
