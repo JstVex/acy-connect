9290ecbfdaada4cbe33c00d2257e3682fa767276
JstVex <heinhtetlulu14@gmail.com>
2023-07-01 15:34:05 +0630

    Fetch and display participated events and all other events

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,86 +1,86 @@
-// import { FC, useEffect, useState } from 'react'
-// import { EventModel, UserModel } from '../types/models';
-// import ParticipatedEvent from '../components/events/ParticipatedEvent';
-// import AllOtherEvents from '../components/events/AllOtherEvents';
+import { FC, useEffect, useState } from 'react'
+import { EventModel, UserModel } from '../types/models';
+import ParticipatedEvent from '../components/events/ParticipatedEvent';
+import AllOtherEvents from '../components/events/AllOtherEvents';
 
-// interface EventsProps {
-//     props?: string
-// }
+interface EventsProps {
+    props?: string
+}
 
-// const Events: FC<EventsProps> = () => {
-//     const [participatedEvents, setParticipatedEvents] = useState<EventModel[]>([]);
-//     const [allOtherEvents, setAllOtherEvents] = useState<EventModel[]>([]);
-//     const [user, setUser] = useState<UserModel | null>(null);
+const Events: FC<EventsProps> = () => {
+    const [participatedEvents, setParticipatedEvents] = useState<EventModel[]>([]);
+    const [allOtherEvents, setAllOtherEvents] = useState<EventModel[]>([]);
+    const [user, setUser] = useState<UserModel | null>(null);
 
-//     useEffect(() => {
-//         const fetchUser = async () => {
-//             try {
-//                 const token = localStorage.getItem('token')
-//                 const response = await fetch('http://localhost:4080/users/me', {
-//                     method: 'GET',
-//                     headers: {
-//                         'Content-Type': 'application/json',
-//                         'Authorization': `Bearer ${token}`,
-//                     },
-//                 });
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const token = localStorage.getItem('token')
+                const response = await fetch('http://localhost:4080/users/me', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`,
+                    },
+                });
 
-//                 if (response.ok) {
-//                     const data = await response.json();
-//                     setUser(data)
-//                     setParticipatedEvents(data.events)
-//                 }
+                if (response.ok) {
+                    const data = await response.json();
+                    setUser(data)
+                    setParticipatedEvents(data.events)
+                }
 
-//             } catch (error) {
-//                 console.error('Error fetching user data:', error);
-//             }
-//         }
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        }
 
-//         const fetchEventsFromAllGroups = async () => {
-//             try {
-//                 const response = await fetch(`http://localhost:4080/events/user/${user?._id}`);
+        const fetchEventsFromAllGroups = async () => {
+            try {
+                const response = await fetch(`http://localhost:4080/events/user/${user?._id}`);
 
-//                 if (response.ok) {
-//                     const data = await response.json();
-//                     console.log('all other events', data)
-//                     setAllOtherEvents(data);
-//                 }
-//             } catch (error) {
-//                 console.error('Error fetching events:', error);
-//             }
-//         }
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log('all other events', data)
+                    setAllOtherEvents(data);
+                }
+            } catch (error) {
+                console.error('Error fetching events:', error);
+            }
+        }
 
-//         fetchUser();
-//         fetchEventsFromAllGroups();
-//     }, [user?._id])
+        fetchUser();
+        fetchEventsFromAllGroups();
+    }, [user?._id])
 
-//     if (user === null) {
-//         return (
-//             <div>
-//                 Loading...
-//             </div>
-//         )
-//     }
+    if (user === null) {
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
 
-//     return (
-//         <div className='w-full max-h-screen overflow-y-auto p-3'>
-//             <h3 className='font-semibold text-lg'>
-//                 Participated Events
-//             </h3>
-//             <ul>
-//                 {participatedEvents.map((event) => {
-//                     return <ParticipatedEvent key={event._id} event={event} user={user} />
-//                 })}
-//             </ul>
-//             <h3 className='font-semibold text-lg'>
-//                 Events happening in groups you have joined
-//             </h3>
-//             <ul>
-//                 {allOtherEvents.map((event) => {
-//                     return <AllOtherEvents key={event._id} event={event} user={user} />
-//                 })}
-//             </ul>
-//         </div>
-//     )
-// }
+    return (
+        <div className='w-full max-h-screen overflow-y-auto p-3'>
+            <h3 className='font-semibold text-lg'>
+                Participated Events
+            </h3>
+            <ul>
+                {participatedEvents.map((event) => {
+                    return <ParticipatedEvent key={event._id} event={event} user={user} />
+                })}
+            </ul>
+            <h3 className='font-semibold text-lg'>
+                Events happening in groups you have joined
+            </h3>
+            <ul>
+                {allOtherEvents.map((event) => {
+                    return <AllOtherEvents key={event._id} event={event} user={user} />
+                })}
+            </ul>
+        </div>
+    )
+}
 
-// export default Events
+export default Events
