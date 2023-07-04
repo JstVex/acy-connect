0f264b487277be9f340e14617c479ecf2f6519f3
JstVex <heinhtetlulu14@gmail.com>
2023-07-04 20:31:41 +0630

    Get only groups user hasn't joined yet and fix owner name
    Use populated owner name instead comparing all users' names to find owner

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,24 +1,15 @@
-import { FC, useEffect, useState } from 'react'
+import { FC } from 'react'
 import Event from './Event'
-import { UserModel } from '../types/models'
-import { GroupModel } from '../types/models'
+import { GroupModel, EventModel } from '../types/models'
 import { AuthUserModel } from '../types/models'
+import { Link } from 'react-router-dom'
 
 interface GroupProps {
     group: GroupModel,
-    users: UserModel[],
     currentUser: AuthUserModel | null
 }
 
-const Group: FC<GroupProps> = ({ group, users, currentUser }) => {
-    const [ownerName, setOwnerName] = useState('');
-
-    useEffect(() => {
-        const owner = users.find((user) => user._id === group.owner);
-        if (owner) {
-            setOwnerName(owner.name);
-        }
-    }, [group.owner, users]);
+const Group: FC<GroupProps> = ({ group, currentUser }) => {
 
     const joinGroup = async () => {
         try {
@@ -43,14 +34,14 @@ const Group: FC<GroupProps> = ({ group, users, currentUser }) => {
     return (
         <li className='flex flex-col ring-1 ring-zinc-900 rounded-md shadow-md my-5 p-3'>
             <div className='flex items-center '>
-                <h3 className='text-lg flex-grow'>
+                <h3 className='text-2xl flex-grow font-semibold'>
                     {group.title}
                 </h3>
                 <div className='font-light'>
-                    Created by {ownerName}
+                    Created by {group.owner.name}
                 </div>
             </div>
-            <p className='my-1'>
+            <p className='text-lg my-1'>
                 {group.description}
             </p>
             <div className='text-sm'>
@@ -59,17 +50,38 @@ const Group: FC<GroupProps> = ({ group, users, currentUser }) => {
             <div className='text-sm'>
                 Usually active from {group.time} on {group.date} at {group.place}
             </div>
-            <div>
-                On going events
+            <div className='flex items-center mt-2 gap-x-2'>
+                {group.events.length === 0 ? (
+                    <div className=''>
+                        This group does not have any ongoing event yet
+                    </div>
+                ) :
+                    <div className=''>
+                        {group.events.length} ongoing events -
+                    </div>
+                }
+                <ul className='flex gap-x-1'>
+                    {group.events.map((event, index) => {
+                        const isLastItem = index === group.events.length - 1;
+                        const comma = isLastItem ? '' : ',';
+                        return (
+                            <li key={event._id}>
+                                {event.title}{comma}
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
-            <div>
-                {group.events.map((event: any) => {
-                    return <Event event={event} />
-                })}
+
+            <div className='flex gap-x-3 items-center ml-auto'>
+                <Link to={`/groups/${group._id}`} className='bg-amber-100 rounded-md px-3 py-2 w-28 mt-4' >
+                    View Group
+                </Link>
+                <button className='bg-amber-100 rounded-md px-3 py-2 w-28 mt-4' onClick={joinGroup}>
+                    Join group
+                </button>
             </div>
-            <button className='bg-amber-100 rounded-md px-3 py-2 w-28 mt-4' onClick={joinGroup}>
-                Join group
-            </button>
+
         </li>
     )
 }
--- src/pages/AllGroups.tsx
+++ src/pages/AllGroups.tsx
@@ -3,7 +3,6 @@ import SearchBar from '../components/all-groups/SearchBar'
 import CreateNewGroup from '../components/all-groups/CreateNewGroup'
 import Group from '../components/Group'
 import { GroupModel } from '../types/models'
-import { UserModel } from '../types/models'
 import { AuthContext } from '../context/AuthContext'
 
 interface AllGroupsProps {
@@ -11,33 +10,23 @@ interface AllGroupsProps {
 }
 
 const AllGroups: FC<AllGroupsProps> = () => {
-    const [groups, setGroups] = useState<GroupModel[] | null>(null);
-    const [users, setUsers] = useState<UserModel[]>([]);
+    const [groups, setGroups] = useState<GroupModel[]>([]);
 
     const { user } = useContext(AuthContext);
 
     useEffect(() => {
         const fetchGroups = async () => {
-            const response = await fetch('http://localhost:4080/groups')
+            const response = await fetch(`http://localhost:4080/groups/unjoined/${user?._id}`)
             const data = await response.json();
 
             if (response.ok) {
                 setGroups(data)
+                console.log('groups are', groups)
             }
         }
 
-        // const fetchUsers = async () => {
-        //     const response = await fetch('http://localhost:4080/users')
-        //     const data = await response.json();
-
-        //     if (response.ok) {
-        //         setUsers(data)
-        //     }
-        // }
-
         fetchGroups();
-        // fetchUsers();
-    }, [groups, users])
+    }, [user?._id])
 
     return (
         <div className='w-full max-h-screen overflow-y-auto p-3'>
@@ -47,7 +36,7 @@ const AllGroups: FC<AllGroupsProps> = () => {
             </div>
             <ul>
                 {groups?.map((group) => {
-                    return <Group key={group._id} group={group} users={users} currentUser={user} />
+                    return <Group key={group._id} group={group} currentUser={user} />
                 })}
             </ul>
         </div>
