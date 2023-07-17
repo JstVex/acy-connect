7b266dffc77e95b9388b43e91ab2c71d103bb72b
JstVex <heinhtetlulu14@gmail.com>
2023-07-17 21:39:16 +0630

    Use tabs to display new and already read notifications separately

--- src/pages/Notifications.tsx
+++ src/pages/Notifications.tsx
@@ -1,14 +1,17 @@
 import { FC, useEffect, useState } from 'react'
 import { UserModel } from '../types/models';
 import Notification from '../components/notifications/Notification';
+import Tabs from '../components/Tabs';
 
 interface NotificationsProps {
     props?: any;
 }
 
 const Notifications: FC<NotificationsProps> = () => {
-    const [user, setUser] = useState<UserModel | null>(null)
-    const [notifications, setNotifications] = useState([])
+    const [user, setUser] = useState<UserModel | null>(null);
+    const [activeTab, setActiveTab] = useState("New Notifications");
+    // const [notifications, setNotifications] = useState([])
+
     useEffect(() => {
         const fetchCurrentUser = async () => {
             try {
@@ -35,19 +38,28 @@ const Notifications: FC<NotificationsProps> = () => {
         fetchCurrentUser();
     }, [])
 
-    if (user === null) {
-        return <div>Loading...</div>;
-    }
+    // if (user === null) {
+    //     return <div>Loading...</div>;
+    // }
     return (
         <div className='w-full max-h-screen overflow-y-auto p-3'>
-            <h2 className='text-3xl font-semibold '>
-                Notifications
-            </h2>
-            <ul className='flex flex-col'>
-                {user.notifications?.map((noti) => {
+            <Tabs activeTab={activeTab} setActiveTab={setActiveTab} firstTitle='New Notifications' secondTitle='Marked as read' >
+                {activeTab === "New Notifications" ? (
+                    user?.notifications?.map((noti) => {
+                        return <Notification notification={noti} user={user} />
+                    })
+
+                ) : (
+                    user?.notifications?.map((noti) => {
+                        return <Notification notification={noti} user={user} />
+                    })
+                )}
+            </Tabs>
+            {/* <ul className='flex flex-col'>
+                {user?.notifications?.map((noti) => {
                     return <Notification notification={noti} user={user} />
                 })}
-            </ul>
+            </ul> */}
         </div>
     )
 }
