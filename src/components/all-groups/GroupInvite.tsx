2b8c76ac3bb865f96b167ffa08806e88de2aa0d0
JstVex <heinhtetlulu14@gmail.com>
2023-07-20 23:06:44 +0630

    Check whether user1 or user2 is the current user to display correct info

--- src/components/all-groups/GroupInvite.tsx
+++ src/components/all-groups/GroupInvite.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { GroupModel, UserModel } from '../../types/models'
 
 interface GroupInviteProps {
@@ -8,6 +8,16 @@ interface GroupInviteProps {
 }
 
 const GroupInvite: FC<GroupInviteProps> = ({ mutual, user, group }) => {
+    const [recipientId, setRecipeintId] = useState('');
+
+    useEffect(() => {
+        if (mutual.user1._id === user._id) {
+            setRecipeintId(mutual.user2._id)
+        } else {
+            setRecipeintId(mutual.user1._id)
+        }
+    }, [mutual.user1._id, mutual.user2._id, user._id])
+
     const handleInvitation = async () => {
         try {
             const response = fetch('http://localhost:4080/users/groupinvitation', {
@@ -15,7 +25,7 @@ const GroupInvite: FC<GroupInviteProps> = ({ mutual, user, group }) => {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ senderId: user._id, recipientId: mutual.user1._id, groupId: group._id })
+                body: JSON.stringify({ senderId: user._id, recipientId: recipientId, groupId: group._id })
             })
         } catch (error) {
             console.log('Error sending invitation', error)
@@ -24,16 +34,30 @@ const GroupInvite: FC<GroupInviteProps> = ({ mutual, user, group }) => {
 
     return (
         <li className='flex my-1'>
-            <div className='flex items-center gap-x-3'>
-                {mutual.user1.image ? (
-                    <img src={mutual.image} alt="" className='w-auto h-auto max-w-[35px] max-h-[35px] aspect-square object-cover rounded-full' />
-                ) : (
-                    <img src="/src/assets/placeholder.jpeg" alt="" className='w-auto h-auto max-w-[30px] max-h-[30px] object-cover aspect-square rounded-full' />
-                )}
-                <div>
-                    {mutual.user1.name}
+            {mutual.user1._id === user._id ? (
+                <div className='flex items-center gap-x-3'>
+                    {mutual.user2.image ? (
+                        <img src={mutual.user2.image} alt="" className='w-auto h-auto max-w-[35px] max-h-[35px] aspect-square object-cover rounded-full' />
+                    ) : (
+                        <img src="/src/assets/placeholder.jpeg" alt="" className='w-auto h-auto max-w-[35px] max-h-[35px] object-cover aspect-square rounded-full' />
+                    )}
+                    <div>
+                        {mutual.user2.name}
+                    </div>
                 </div>
-            </div>
+            ) : (
+                <div className='flex items-center gap-x-3'>
+                    {mutual.user1.image ? (
+                        <img src={mutual.user1.image} alt="" className='w-auto h-auto max-w-[35px] max-h-[35px] aspect-square object-cover rounded-full' />
+                    ) : (
+                        <img src="/src/assets/placeholder.jpeg" alt="" className='w-auto h-auto max-w-[35px] max-h-[35px] object-cover aspect-square rounded-full' />
+                    )}
+                    <div>
+                        {mutual.user1.name}
+                    </div>
+                </div>
+            )}
+
             <div className='ml-auto text-amber-600 cursor-pointer' onClick={handleInvitation}>
                 invite
             </div>
