c7969bf5a363b567d27efff400915b3d72fba1ad
JstVex <heinhtetlulu14@gmail.com>
2023-07-20 23:04:09 +0630

    Fix the wrong filtering logic

--- src/components/all-groups/GroupInvitationModal.tsx
+++ src/components/all-groups/GroupInvitationModal.tsx
@@ -13,17 +13,16 @@ interface GroupInvitationModalProps {
 }
 
 const GroupInvitationModal: FC<GroupInvitationModalProps> = ({ isOpen, onClose, mutuals, user, currentGroup }) => {
-    // const filteredMutuals = mutuals?.filter((mutual) => mutual.user1.groups?.every((group: any) => group !== currentGroup._id));
     const filteredMutuals = mutuals?.filter((mutual) => {
         const isCurrentUserUser1 = mutual.user1._id === user._id;
         const isCurrentUserUser2 = mutual.user2._id === user._id;
 
         if (isCurrentUserUser1) {
-            return !mutual.user2.groups.some((group: any) => group._id === currentGroup._id);
+            return mutual.user2.groups?.every((group: any) => group !== currentGroup._id)
         }
 
         if (isCurrentUserUser2) {
-            return !mutual.user1.groups?.some((group: any) => group._id === currentGroup._id);
+            return mutual.user1.groups?.every((group: any) => group !== currentGroup._id);
         }
 
         return false;
@@ -34,12 +33,16 @@ const GroupInvitationModal: FC<GroupInvitationModalProps> = ({ isOpen, onClose, 
             <h3 className='text-2xl font-semibold leading-7 text-gray-900'>
                 Invite friends
             </h3>
+            {filteredMutuals?.length === 0 && (
+                <div className='mt-3 font-light'>
+                    All of your friends are already in this group.
+                </div>
+            )}
             <ul className='mt-5 mb-3'>
                 {filteredMutuals?.map((mutual) => {
                     return <GroupInvite key={mutual._id} mutual={mutual} user={user} group={currentGroup} />
                 })}
             </ul>
-
         </Modal>
     )
 }
