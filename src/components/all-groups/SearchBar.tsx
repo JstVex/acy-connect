f8c31b66a5684d265da886c2e12faee2ff3e8a4f
JstVex <heinhtetlulu14@gmail.com>
2023-07-24 22:00:57 +0630

    Modify to allow filtering events as well

--- src/components/all-groups/SearchBar.tsx
+++ src/components/all-groups/SearchBar.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, FC, SetStateAction } from 'react'
-import { GroupModel, UserModel } from '../../types/models';
+import { EventModel, GroupModel, UserModel } from '../../types/models';
 import { Search } from 'lucide-react';
 
 interface ConnectionModel {
@@ -8,10 +8,10 @@ interface ConnectionModel {
 }
 
 interface SearchBarProps {
-    setFiltered: Dispatch<SetStateAction<GroupModel[]>> | Dispatch<SetStateAction<UserModel[]>>;
-    setFiltered2?: Dispatch<SetStateAction<ConnectionModel[]>> | Dispatch<SetStateAction<GroupModel[]>>;
-    filterField: GroupModel[] | UserModel[];
-    filterField2?: ConnectionModel[] | GroupModel[];
+    setFiltered: Dispatch<SetStateAction<GroupModel[]>> | Dispatch<SetStateAction<UserModel[]>> | Dispatch<SetStateAction<EventModel[]>>;
+    setFiltered2?: Dispatch<SetStateAction<ConnectionModel[]>> | Dispatch<SetStateAction<GroupModel[]>> | Dispatch<SetStateAction<EventModel[]>>;
+    filterField: GroupModel[] | UserModel[] | EventModel[];
+    filterField2?: ConnectionModel[] | GroupModel[] | EventModel[];
     setSearchValue: Dispatch<SetStateAction<string>>;
     filterBy: string;
     placeholder: string;
@@ -24,26 +24,45 @@ const SearchBar: FC<SearchBarProps> = ({ setFiltered, setFiltered2, filterField,
 
         if (value.trim() === '') {
             setFiltered([]);
+            if (setFiltered2)
+                setFiltered2([]);
         } else {
             if (filterBy === 'title') {
                 const filtered = (filterField as GroupModel[]).filter((field) => field.title.toLowerCase().includes(value.toLowerCase()));
-                setFiltered(filtered as SetStateAction<GroupModel[]> & SetStateAction<UserModel[]>);
+                setFiltered(filtered as SetStateAction<GroupModel[]> & SetStateAction<UserModel[]> &
+                    SetStateAction<EventModel[]>);
                 if (setFiltered2) {
                     const filtered2 = (filterField2 as GroupModel[]).filter((field) => field.title.toLowerCase().includes(value.toLowerCase()));
                     setFiltered2(filtered2 as SetStateAction<GroupModel[]> &
-                        SetStateAction<ConnectionModel[]>);
+                        SetStateAction<ConnectionModel[]> &
+                        SetStateAction<EventModel[]>);
                 }
             }
 
             if (filterBy === 'name') {
                 const filtered = (filterField as UserModel[]).filter((field) => field.name.toLowerCase().includes(value.toLowerCase()));
-                setFiltered(filtered as SetStateAction<GroupModel[]> & SetStateAction<UserModel[]>);
+                setFiltered(filtered as SetStateAction<GroupModel[]> & SetStateAction<UserModel[]> &
+                    SetStateAction<EventModel[]>);
                 if (setFiltered2) {
                     const filtered2 = (filterField2 as ConnectionModel[]).filter((field) => field.connectedUser.name.toLowerCase().includes(value.toLowerCase()));
                     setFiltered2(filtered2 as SetStateAction<GroupModel[]> &
-                        SetStateAction<ConnectionModel[]>);
+                        SetStateAction<ConnectionModel[]> &
+                        SetStateAction<EventModel[]>);
                 }
 
+            }
+
+            if (filterBy === 'event title') {
+                const filtered = (filterField as EventModel[]).filter((field) => field.title.toLowerCase().includes(value.toLowerCase()));
+                setFiltered(filtered as SetStateAction<GroupModel[]> & SetStateAction<UserModel[]> &
+                    SetStateAction<EventModel[]>);
+
+                if (setFiltered2) {
+                    const filtered2 = (filterField2 as EventModel[]).filter((field) => field.title.toLowerCase().includes(value.toLowerCase()));
+                    setFiltered2(filtered2 as SetStateAction<GroupModel[]> &
+                        SetStateAction<ConnectionModel[]> &
+                        SetStateAction<EventModel[]>);
+                }
             }
 
         }
@@ -58,11 +77,12 @@ const SearchBar: FC<SearchBarProps> = ({ setFiltered, setFiltered2, filterField,
             <label htmlFor="search" className="sr-only">
                 Search
             </label>
-            <div className="flex items-center border border-zinc-900 rounded-full py-2 px-3 w-full">
+            <div className="flex items-center border border-gray-100 rounded-full py-2 px-3 w-full shadow-sm bg-white">
                 <Search size={20} className='text-gray-600' />
                 <input
                     type="text"
                     id="search"
+                    autoComplete='off'
                     className="ml-2 flex-grow bg-transparent placeholder-zinc-600 focus:outline-none"
                     placeholder={placeholder}
                     onChange={handleSearchInputChange}
