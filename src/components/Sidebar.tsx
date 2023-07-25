3b34a29905e697cf31648d91e679e1f0e41f0833
JstVex <heinhtetlulu14@gmail.com>
2023-07-25 19:50:53 +0630

    Close the sidebar when a link is clicked on mobile screen

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,15 +3,18 @@ import { useEffect, useState } from 'react';
 import { UserModel } from '../types/models';
 import clsx from 'clsx';
 import { X, Menu } from 'lucide-react';
+import useWindowWidth from '../hooks/useWindowWidth';
 
 function Sidebar() {
     const [user, setUser] = useState<UserModel | null>(null);
 
-    const [isOpen, setIsOpen] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
+
+    const windowWidth = useWindowWidth();
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -39,30 +42,64 @@ function Sidebar() {
     }, [])
 
     return (
-        <div className={clsx('relative flex')}>
+        <div className={clsx('relative flex z-50')}>
             <nav className={clsx('h-screen shadow-sm shadow-amber-950 bg-amber-100 pt-3 flex flex-col', isOpen ? 'absolute sm:relative w-screen sm:w-60' : 'w-0', 'transition-all duration-300 ease-in-out')}>
                 <h4 className={clsx('text-2xl px-3 text-amber-800 mb-5 font-semibold', isOpen ? 'opacity-100' : 'opacity-0', 'transition-opacity duration-100 ease-in-out')}>
                     Acy Connect
                 </h4>
                 <ul className={clsx('flex flex-col text-lg px-3', isOpen ? 'opacity-100' : 'opacity-0', 'transition-opacity duration-100 ease-in-out')}>
                     <li className="py-1 border-b-2 border-amber-800">
-                        <Link to="/groups">All Groups</Link>
+                        <Link to="/groups" onClick={() => {
+                            if (windowWidth < 640) {
+                                setIsOpen(false);
+                            }
+                        }}>
+                            All Groups
+                        </Link>
                     </li>
                     <li className="py-1 border-b-2 border-amber-800">
-                        <Link to="/my-groups">My Groups</Link>
+                        <Link to="/my-groups" onClick={() => {
+                            if (windowWidth < 640) {
+                                setIsOpen(false);
+                            }
+                        }}>
+                            My Groups
+                        </Link>
                     </li>
                     <li className="py-1 border-b-2 border-amber-800">
-                        <Link to="/connections">Connections</Link>
+                        <Link to="/connections" onClick={() => {
+                            if (windowWidth < 640) {
+                                setIsOpen(false);
+                            }
+                        }}>
+                            Connections
+                        </Link>
                     </li>
                     <li className="py-1 border-b-2 border-amber-800">
-                        <Link to="/events">Events</Link>
+                        <Link to="/events" onClick={() => {
+                            if (windowWidth < 640) {
+                                setIsOpen(false);
+                            }
+                        }}>
+                            Events
+                        </Link>
                     </li>
                     <li className="py-1">
-                        <Link to="/notifications">Notifications</Link>
+                        <Link to="/notifications" onClick={() => {
+                            if (windowWidth < 640) {
+                                setIsOpen(false);
+                            }
+                        }}>
+                            Notifications
+                        </Link>
                     </li>
                 </ul>
 
-                <Link className={clsx('mt-auto flex items-center py-2 px-3 gap-x-3 bg-amber-900', isOpen ? 'opacity-100' : 'opacity-0', 'transition-opacity duration-100 ease-in-out')} to="/me">
+                <Link className={clsx('mt-auto flex items-center py-2 px-3 gap-x-3 bg-amber-900', isOpen ? 'opacity-100' : 'opacity-0', 'transition-opacity duration-100 ease-in-out')} to="/me" onClick={() => {
+                    if (windowWidth < 640) {
+                        setIsOpen(false);
+                    }
+                }}>
                     {user?.image ? (
                         <img src={user?.image} alt="pfp" className='w-auto h-auto max-w-[48px] max-h-[48px] aspect-square object-cover rounded-full' />
                     ) : (
@@ -92,7 +129,7 @@ function Sidebar() {
             <button
                 className={clsx(
                     'absolute shadow-sm shadow-amber-950 -translate-y-1/2 transform mt-auto p-1 rounded-full transition-all duration-300 ease-in-out',
-                    isOpen ? 'hidden sm:block' : '',
+                    isOpen ? 'hidden sm:block' : 'hover:-right-1',
                     'translate-x-full text-amber-100 bg-amber-800 right-4',
                     'top-5',
                     'z-10'
