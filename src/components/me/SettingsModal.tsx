90d6aa30b514e6943859decc9a9f7e04a5909b23
JstVex <heinhtetlulu14@gmail.com>
2023-07-25 15:22:09 +0630

    Modify the form to allow image upload using formData

--- src/components/me/SettingsModal.tsx
+++ src/components/me/SettingsModal.tsx
@@ -16,12 +16,14 @@ const SettingsModal: FC<SettingsModalProps> = ({ isOpen, setIsOpen, onClose, use
     const [profile, setProfile] = useState({
         email: user?.email,
         name: user?.name,
-        image: user?.image,
+        image: '',
         bio: user?.bio,
         fblink: user?.facebookLink,
         hobbies: user?.hobbies,
         activeDay: user?.activeDay,
     });
+
+    const [selectedImage, setSelectedImage] = useState<any>(null);
 
     const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
@@ -31,16 +33,36 @@ const SettingsModal: FC<SettingsModalProps> = ({ isOpen, setIsOpen, onClose, use
         }));
     };
 
+    const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        setSelectedImage(file);
+        if (file) {
+            setProfile((prevProfile: any) => ({
+                ...prevProfile,
+                image: file,
+            }));
+        }
+    };
+
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         try {
+            const formData = new FormData();
+
+            Object.keys(profile).forEach((key) => {
+                if (key !== 'image') {
+                    formData.append(key, profile[key]);
+                }
+            });
+
+            if (profile.image) {
+                formData.append('image', profile.image);
+            }
+
             const response = await fetch('http://localhost:4080/users/me', {
                 method: 'PATCH',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ profile: profile })
+                body: formData
             });
 
             if (response.ok) {
@@ -62,25 +84,59 @@ const SettingsModal: FC<SettingsModalProps> = ({ isOpen, setIsOpen, onClose, use
             <p className="mt-1 text-sm leading-6 text-gray-600">
                 Customize your public information for others to see you
             </p>
-            <form className='mt-6 flex flex-col gap-y-0' onSubmit={handleSubmit}>
-                <ProfileInput
-                    id='name'
-                    name='name'
-                    label='Name'
-                    type='text'
-                    value={profile.name}
-                    onChange={handleInputChange}
-                    placeholder='Name'
-                />
-                <ProfileInput
-                    id='image'
-                    name='image'
-                    label='Image'
-                    type='text'
-                    value={profile.image}
-                    onChange={handleInputChange}
-                    placeholder='Image'
-                />
+            <form className='mt-4 flex flex-col gap-y-0' onSubmit={handleSubmit} encType="multipart/form-data">
+                <div className='flex flex-col sm:flex-row sm:items-center gap-x-6'>
+                    <ProfileInput
+                        id='name'
+                        name='name'
+                        label='Name'
+                        type='text'
+                        value={profile.name}
+                        onChange={handleInputChange}
+                        placeholder='Name'
+                        className='order-last sm:order-first'
+                    />
+                    <div className='flex items-center gap-x-5'>
+                        <div className='my-1 order-last sm:order-first'>
+                            <label htmlFor="image" className="block text-sm font-medium leading-6 text-zinc-900">
+                                Profile Picture
+                            </label>
+                            <div className="relative mt-1">
+                                <input
+                                    id="image"
+                                    name="image"
+                                    type="file"
+                                    onChange={handleImageChange}
+                                    className="opacity-0 absolute top-0 right-0 cursor-pointer w-full h-full"
+                                />
+                                <label htmlFor="image" className="block w-full rounded-md border-0 py-1.5 px-2 text-zinc-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-zinc-400 focus:outline-none focus:ring-1 focus:ring-inset focus:ring-amber-500 sm:text-sm sm:leading-6">
+                                    Choose Image
+                                </label>
+                            </div>
+                        </div>
+
+                        {selectedImage ? (
+                            <img
+                                src={URL.createObjectURL(selectedImage)}
+                                alt=""
+                                className="w-auto h-auto max-w-[70px] max-h-[70px] aspect-square object-cover rounded-full"
+                            />
+                        ) : user?.image ? (
+                            <img
+                                src={user.image}
+                                alt=""
+                                className="w-auto h-auto max-w-[75px] max-h-[75px] aspect-square object-cover rounded-full"
+                            />
+                        ) : (
+                            <img
+                                src="/src/assets/placeholder.jpeg"
+                                alt=""
+                                className="w-auto h-auto max-w-[75px] max-h-[75px] aspect-square object-cover rounded-full"
+                            />
+                        )}
+                    </div>
+
+                </div>
                 <ProfileInput
                     id='bio'
                     name='bio'
@@ -97,7 +153,7 @@ const SettingsModal: FC<SettingsModalProps> = ({ isOpen, setIsOpen, onClose, use
                     type='text'
                     value={profile.hobbies}
                     onChange={handleInputChange}
-                    placeholder='Hobbies'
+                    placeholder='Eg. Drawing, Reading'
                 />
                 <ProfileInput
                     id='activeDay'
@@ -106,7 +162,7 @@ const SettingsModal: FC<SettingsModalProps> = ({ isOpen, setIsOpen, onClose, use
                     type='text'
                     value={profile.activeDay}
                     onChange={handleInputChange}
-                    placeholder='Active day'
+                    placeholder='Eg. Wednesday, Friday'
                 />
                 <ProfileInput
                     id='fblink'
@@ -117,7 +173,7 @@ const SettingsModal: FC<SettingsModalProps> = ({ isOpen, setIsOpen, onClose, use
                     onChange={handleInputChange}
                     placeholder='Facebook link'
                 />
-                <button type='submit' className='bg-amber-100 px-2 py-1.5 rounded-md mt-5 w-20'>
+                <button type='submit' className='ml-auto bg-amber-800 text-white px-2 py-1.5 rounded-md mt-5 w-20'>
                     Confirm
                 </button>
             </form>
